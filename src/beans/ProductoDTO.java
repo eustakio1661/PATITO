af2939824bbca4096fee50be3bf0e7f4cd0644c2
diegogu <diegogu@gmail.com>
2021-05-05 13:06:10 -0500

    Modificando ProductoDTO y agregando la clase CategoriaDTO

--- src/beans/ProductoDTO.java
+++ src/beans/ProductoDTO.java
@@ -1,5 +1,52 @@
 package beans;
 
 public class ProductoDTO {
-	private int cod;
+	private int codidgo,cantidad,idcategoria;
+	private String descripcion,imagen;
+	private double precio;
+	
+	public ProductoDTO() {
+		
+	}
+	
+	public int getCodidgo() {
+		return codidgo;
+	}
+	public void setCodidgo(int codidgo) {
+		this.codidgo = codidgo;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	public int getIdcategoria() {
+		return idcategoria;
+	}
+	public void setIdcategoria(int idcategoria) {
+		this.idcategoria = idcategoria;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getImagen() {
+		return imagen;
+	}
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	
+	
+	
+
 }
