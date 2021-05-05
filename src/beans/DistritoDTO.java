1095f8dca894b7e409b163e5f3f1741692cf95f0
Ricardo Rios <ricardos_92@hotmail.com>
2021-05-05 13:45:15 -0500

    Agregando Distrito y Tipo Cliente

---
+++ src/beans/DistritoDTO.java
@@ -0,0 +1,22 @@
+package beans;
+
+public class DistritoDTO {
+	
+	private int iddistrito;
+	private String desdistrito;
+	public int getIddistrito() {
+		return iddistrito;
+	}
+	public void setIddistrito(int iddistrito) {
+		this.iddistrito = iddistrito;
+	}
+	public String getDesdistrito() {
+		return desdistrito;
+	}
+	public void setDesdistrito(String desdistrito) {
+		this.desdistrito = desdistrito;
+	}
+	
+	
+
+}
