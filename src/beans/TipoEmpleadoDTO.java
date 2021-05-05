c31a903b2e93bc180949daa4eea084657f88d7f5
Ricardo Rios <ricardos_92@hotmail.com>
2021-05-05 14:32:15 -0500

    Arreglando Tipo Empleado

--- src/beans/TipoEmpleadoDTO.java
+++ src/beans/TipoEmpleadoDTO.java
@@ -1,27 +1,29 @@
 package beans;
 
-public class TipoClienteDTO {
-	
+public class TipoEmpleadoDTO {
+
 	private int id;
 	private String descripcion;
-	
-	public TipoClienteDTO(int id, String descripcion) {
+
+	public TipoEmpleadoDTO(int id, String descripcion) {
 		this.id = id;
 		this.descripcion = descripcion;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-	
-	
 
 }
