ea9966d56665406e8eb795a3fbf32c342dad6555
Ricardo Rios <ricardos_92@hotmail.com>
2021-05-05 14:31:35 -0500

    Arreglando Distrito

--- src/beans/DistritoDTO.java
+++ src/beans/DistritoDTO.java
@@ -1,28 +1,29 @@
 package beans;
 
 public class DistritoDTO {
-	
+
 	private int id;
 	private String descripcion;
-	
+
 	public DistritoDTO(int id, String descripcion) {
 		this.id = id;
 		this.descripcion = descripcion;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-	
-	
-	
 
 }
