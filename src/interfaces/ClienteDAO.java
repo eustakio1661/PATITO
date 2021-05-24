44ff658784eaf8f3b018b7ea018a4564894edb16
Ricardo <ricardos_92@hotmail.com>
2021-05-24 01:00:24 -0500

    Añadiendo Cliente Interface

---
+++ src/interfaces/ClienteDAO.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+import java.util.ArrayList;
+
+import beans.ClienteDTO;
+
+public interface ClienteDAO {
+
+	public ArrayList<ClienteDTO> listarCliente();
+	public int registrarCliente(ClienteDTO cli);
+	public int actualizarCliente(ClienteDTO cli);
+	public ClienteDTO buscarCliente(String cod);
+	
+}
