04a8922090150178359a81502ea3ec9e1b22cec0
jossrc <kuro.rc10@gmail.com>
2021-05-31 13:22:17 -0500

    Borrando importación innecesaria

--- src/mantenimiento/MySQLProductoDAO.java
+++ src/mantenimiento/MySQLProductoDAO.java
@@ -3,7 +3,6 @@ package mantenimiento;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 
 import beans.CategoriaDTO;
