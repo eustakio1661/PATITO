ba092bdfc9455ba2bb6e54e27b8c7f7fe8d54d00
dacho <admin@gmail.com>
2021-06-02 10:56:30 -0500

    Modificando EmpleadoDAO

--- src/interfaces/EmpleadoDAO.java
+++ src/interfaces/EmpleadoDAO.java
@@ -8,7 +8,8 @@ public interface EmpleadoDAO {
     
     public int registrar(EmpleadoDTO e);
     public ArrayList<EmpleadoDTO> listado();
-    public int actualizar(EmpleadoDTO e);
+    public int actualizarEmpleado(EmpleadoDTO e);
     public int eliminar(EmpleadoDTO e);
     public EmpleadoDTO validarAcceso(String correo_usuario, String clave);
+    public ArrayList<EmpleadoDTO> listadoTipoEmpleado();
 }
