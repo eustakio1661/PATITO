3612af39cf7ffa0368289aebfc0600fca34beabd
jossrc <kuro.rc10@gmail.com>
2021-06-03 12:05:17 -0500

    Preparando el primer archivo para enviar form

---
+++ WebContent/js/send_form.js
@@ -0,0 +1,59 @@
+const myForm = document.getElementById('myForm');
+
+const enviarFormulario = (form) => {
+  const formData = new FormData(form);
+  const action = form.getAttribute('action');
+  const method = form.method;
+
+  fetch(action, {
+    method,
+    body: formData,
+  })
+  .then((resp) => resp.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+};
+
+// ${objeto} ${txtDescripcionProd.value}
+const mostrarAlertRegistro = (form) => {
+  Swal.fire({
+    title: `Registrar Cliente`,
+    text: `\u00bfDesea registrar el Cliente a la BD?`,
+    icon: 'question',
+    showCancelButton: true,
+    confirmButtonText: 'Aceptar',
+    cancelButtonText: 'Cancelar',
+  }).then(boton => {
+    if (boton.value) {
+      enviarFormulario(form);
+    }
+  })
+}
+
+// Valida los formularios
+(function () {
+  'use strict';
+
+  var forms = document.querySelectorAll('.needs-validation');
+
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      'submit',
+      function (event) {
+        event.preventDefault();
+        if (!form.checkValidity()) {
+          event.stopPropagation();
+        } else {
+          mostrarAlertRegistro(form);
+        }
+
+        form.classList.add('was-validated');
+      },
+      false
+    );
+  });
+})();
