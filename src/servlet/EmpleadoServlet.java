fff64d8164acf39868dd678bc69036114fef1193
jossrc <kuro.rc10@gmail.com>
2021-06-04 01:30:10 -0500

    Agregando el multipartconfig para que se comunique con js

--- src/servlet/EmpleadoServlet.java
+++ src/servlet/EmpleadoServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import beans.EmpleadoDTO;
 import dao.DAOFactory;
 
+@MultipartConfig
 @WebServlet(name = "emse", urlPatterns = { "/emse" })
 public class EmpleadoServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
