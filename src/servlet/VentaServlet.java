985c6d0739e0770e44782d28c7817f1a9060a195
diegogu <diegoagutarrac@gmail.com>
2021-06-05 10:39:17 -0500

    Agregando los metodos necesarios para comprar producto

--- src/servlet/VentaServlet.java
+++ src/servlet/VentaServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,8 +10,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import beans.BoletaDTO;
 import beans.ClienteDTO;
-import beans.Detalle_PedidoDTO;
+import beans.DetallePedidoDTO;
+import beans.EmpleadoDTO;
+import beans.PedidoDTO;
 import beans.ProductoDTO;
 import dao.DAOFactory;
 import interfaces.ClienteDAO;
@@ -27,7 +31,7 @@ public class VentaServlet extends HttpServlet {
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    System.out.println("Entró al Servlet de Producto");
+	    System.out.println("Entró al Servlet de Venta");
         String opcion = request.getParameter("opcion") != null ? request.getParameter("opcion") : "error";
 
         try {
@@ -47,23 +51,69 @@ public class VentaServlet extends HttpServlet {
             case "eliCompra":
                 eliminarCompraCarro(request, response);
                 break;
+            case "procCompra":
+                procesarCompra(request, response);
+                break;
             default:
                 System.out.println("Error en la opcion");
                 break;
             }
         } catch (Exception e) {
             // response.sendRedirect("error.jsp");
-            System.out.println("Error inesperado en el Producto Servlet");
+            System.out.println("Error inesperado en la Venta Servlet");
+            System.out.println(e);
         }
 	}
 
+    private void procesarCompra(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Entrando a Procesar Compra");
+        EmpleadoDTO em = (EmpleadoDTO) request.getSession().getAttribute("e");
+        ClienteDTO cl = (ClienteDTO) request.getSession().getAttribute("clienteEncontrado");
+        int cantidadProductos = (int) request.getSession().getAttribute("cantidadProductos");
+        double subTotalVentas = (double) request.getSession().getAttribute("subTotalVentas");
+        ArrayList<DetallePedidoDTO> carro = (ArrayList<DetallePedidoDTO>) request.getSession().getAttribute("carro");
+        PedidoDTO pe = new PedidoDTO();
+        pe.setId_pe(generarNumPedido());
+        pe.setId_em(em.getId());
+        pe.setId_cli(cl.getCodigo());
+        pe.setCantidadTotal(cantidadProductos);
+     
+        BoletaDTO bo = new BoletaDTO();
+        bo.setId_bol(generarNumBoleta());
+        bo.setPrecioTotal(subTotalVentas);
+        
+        DAOFactory fabrica = DAOFactory.getDAOFactory(DAOFactory.MYSQL);
+        int rs = fabrica.getVentaDao().realizarVenta(pe, carro, bo);
+        if(rs ==  0) {
+            System.out.println("Error en la transaccion");
+        }else {
+            System.out.println("Transaccion Exitosa");
+            //Reiniciar Variable Globales a nivel de Session
+            request.getSession().setAttribute("carro", new ArrayList<DetallePedidoDTO>());
+            request.getSession().setAttribute("cantidadProductos", 0);
+            request.getSession().setAttribute("subTotalVentas", 0.00);
+        }
+        
+        request.getRequestDispatcher("home.jsp").forward(request, response);
+    }
+
+    private int generarNumPedido() {
+        DAOFactory fabrica = DAOFactory.getDAOFactory(DAOFactory.MYSQL);
+        return fabrica.getVentaDao().generarNumPedido();
+    }
+
+    private String generarNumBoleta() {
+        DAOFactory fabrica = DAOFactory.getDAOFactory(DAOFactory.MYSQL);
+        return fabrica.getVentaDao().generaNumBoleta();
+    }
+
     private void eliminarCompraCarro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ArrayList<Detalle_PedidoDTO> carro = (ArrayList<Detalle_PedidoDTO>) request.getSession().getAttribute("carro");
+        ArrayList<DetallePedidoDTO> carro = (ArrayList<DetallePedidoDTO>) request.getSession().getAttribute("carro");
         int cantidadProductos = (int) request.getSession().getAttribute("cantidadProductos");
         double subTotalVentas = (double) request.getSession().getAttribute("subTotalVentas");
        
         int idprod = Integer.parseInt(request.getParameter("codigo"));
-        for (Detalle_PedidoDTO d : carro) {
+        for (DetallePedidoDTO d : carro) {
             if (d.getId_pro() == idprod) {
                 cantidadProductos -= d.getCantidad();
                 subTotalVentas -= d.getImporte();
@@ -96,12 +146,12 @@ public class VentaServlet extends HttpServlet {
         int cantidad = Integer.parseInt(request.getParameter("txtCantidad"));
         ProductoDTO p = (ProductoDTO) request.getSession().getAttribute("productoEncontrado");
         //Traer atributos de session
-        ArrayList<Detalle_PedidoDTO> carro = (ArrayList<Detalle_PedidoDTO>) request.getSession().getAttribute("carro");
+        ArrayList<DetallePedidoDTO> carro = (ArrayList<DetallePedidoDTO>) request.getSession().getAttribute("carro");
         int cantidadProductos = (int) request.getSession().getAttribute("cantidadProductos");
         double subTotalVentas = (double) request.getSession().getAttribute("subTotalVentas");
-        
+        System.out.println(p.getIdProducto());
         //Agregamos al carro y actualizamos los acumuladores
-        Detalle_PedidoDTO detalle = new Detalle_PedidoDTO();
+        DetallePedidoDTO detalle = new DetallePedidoDTO();
         detalle.setId_pro(p.getIdProducto());
         detalle.setCantidad(cantidad);
         detalle.setPrecio(p.getPrecio());
@@ -110,10 +160,12 @@ public class VentaServlet extends HttpServlet {
         detalle.setNombreProd(p.getDescripcion());
         detalle.setImporte(cantidad * p.getPrecio());
        
+        carro.add(detalle);
+        
         cantidadProductos += cantidad;
         subTotalVentas += detalle.getImporte();   
         
-        carro.add(detalle);
+       
     
         //Envia las variables globales a nivel de session
         request.getSession().setAttribute("carro", carro);
@@ -125,7 +177,7 @@ public class VentaServlet extends HttpServlet {
     }
 
     private void listadoClienteyProducto(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("Ingreso al proceso ListarProducto");
+        System.out.println("Ingreso al proceso ListarClienteProducto");
 
         DAOFactory fabrica = DAOFactory.getDAOFactory(DAOFactory.MYSQL);
         ClienteDAO dao = fabrica.getClienteDAO();
@@ -133,9 +185,10 @@ public class VentaServlet extends HttpServlet {
         ArrayList<ClienteDTO> lista = dao.listarClientexDistrito();
         ArrayList<ProductoDTO> lista1 = dao1.listado();
 
-        request.setAttribute("lstProductos", lista1);
+      
         request.setAttribute("lstClientes", lista);
-        request.getRequestDispatcher("/catalogo.jsp").forward(request, response);
+        request.setAttribute("lstProductos", lista1);
+        request.getRequestDispatcher("catalogo.jsp").forward(request, response);
         
     }
 
