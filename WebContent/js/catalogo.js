235658ae48289082ce6a1ee5699f0601e304b98f
jossrc <kuro.rc10@gmail.com>
2021-06-07 21:22:53 -0500

    Obteniendo los datos de una fila

--- WebContent/js/catalogo.js
+++ WebContent/js/catalogo.js
@@ -1,13 +1,17 @@
+const getDatosXFila = (fila) => {
+  const celdas = Array.from(fila.cells).slice(1,-1);
+  console.log(celdas);
+  const datos = celdas.map( celda => celda.innerText );
+  console.log(datos);
+}
 
 const btnsSelectProd  = document.querySelectorAll('.select-prod');
-
 
 if (btnsSelectProd.length > 0) {
   btnsSelectProd.forEach((btn) => {
     btn.addEventListener('click', () => {
       const filaElement = btn.parentElement.parentElement.parentElement;
-      console.log('fila : ', filaElement);
-      console.log('num fila : ', filaElement.rowIndex);
+      getDatosXFila(filaElement);
     });
   });
 }
