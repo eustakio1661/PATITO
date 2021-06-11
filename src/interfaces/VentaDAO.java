0b4a1ccfbedbad4f92744c84a2184560539ab81a
diegogu <diegoagutarrac@gmail.com>
2021-06-11 13:22:47 -0500

    Metodos para la venta

---
+++ src/interfaces/VentaDAO.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+import java.util.ArrayList;
+
+import beans.BoletaDTO;
+import beans.DetallePedidoDTO;
+import beans.PedidoDTO;
+
+public interface VentaDAO {
+    public String generaNumBoleta();
+    public int generarNumPedido();
+    public int realizarVenta(PedidoDTO p, ArrayList<DetallePedidoDTO> det, BoletaDTO b);
+}
