58c3d0fc4ba4a2b04c49c3a13168fe3336302d7d
diegogu <diegoagutarrac@gmail.com>
2021-06-20 16:04:17 -0500

    Agregando metodo reporte Cliente

--- src/interfaces/ReporteDAO.java
+++ src/interfaces/ReporteDAO.java
@@ -3,9 +3,10 @@ package interfaces;
 import java.util.ArrayList;
 
 import beans.ListadoEntreFechasDTO;
+import beans.ReporteClienteDTO;
 
 public interface ReporteDAO {
     public ArrayList<ListadoEntreFechasDTO> reporte(String fecha1, String fecha2);
     public ArrayList<ListadoEntreFechasDTO> lista();
-
+    public ArrayList<ReporteClienteDTO> reporteCliente();
 }
