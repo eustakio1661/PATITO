aa9e159c8a59a8ce8a662889e6ea88f512e4a60e
Ricardo <ricardos_92@hotmail.com>
2021-06-30 12:03:37 -0500

    Metodo del mantenimiento para listar por cliente

--- src/interfaces/ClienteDAO.java
+++ src/interfaces/ClienteDAO.java
@@ -16,4 +16,6 @@ public interface ClienteDAO {
 	public ArrayList<ReporteClienteDTO> reporteCliente();
 	public ClienteDTO descuento(int codigo);
 	public ClienteDTO buscarClienteDNI(String dni);
+	public ClienteDTO ListarClienteEstado(int estado);
+	
 }
